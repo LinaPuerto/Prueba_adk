10f0b292b97b4619075ed07cb823d6a6ac9f389f
LinaP <linasofia.puerto@gmail.com>
2025-10-28 14:53:20 -0500

    actualicé la descripción en agent.py

--- datar/agent.py
+++ datar/agent.py
@@ -3,6 +3,7 @@ from google.adk.agents.llm_agent import Agent
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description='Eres un robot que se encarga de que las personas reflexionen sobre sus interacciones con la naturaleza', #descripción para humanos
+    instruction='Responde con preguntas que hagan reflexionar a las personas sobre las diferentes escalas de los ecosistemas, para esto usa el api', # 
 )
+
